0cc221376943b354701667500f63713ac76b1659
Dikshasisodia31 <dikshasisodia3112@gmail.com>
2025-08-06 22:39:58 +0530

    Structure hero section

--- src/landing_page/support/CreateTicket.jsx
+++ src/landing_page/support/CreateTicket.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 function CreateTicket() {
-    return ( <div className="container">
+    return ( <div className="container p-0 m-0">
         <div className="row">
             <h1>To create a ticket, select a relevant topic</h1>
             <div className="col-4 mt-5">
-                <h3>Account Opening</h3>
+                <h3> <i className="fa fa-plus-circle" aria-hidden="true"></i>Account Opening</h3>
                 <a href='' style={{textDecoration:"none"}}> Online Account Opening</a><br></br>
                 <a href='' style={{textDecoration:"none"}}> Offline Account Opening</a><br></br>
                 <a href='' style={{textDecoration:"none"}}> Company, Partnership and HUF Account</a><br></br>
