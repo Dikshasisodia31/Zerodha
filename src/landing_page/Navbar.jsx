5e01fba2cc14848078ee21d90571408ff6fee85c
Dikshasisodia31 <dikshasisodia3112@gmail.com>
2025-08-08 23:09:58 +0530

    Structure Navbar

--- src/landing_page/Navbar.jsx
+++ src/landing_page/Navbar.jsx
@@ -4,39 +4,34 @@ import '../index.css'
 import { Link } from "react-router-dom";
 function Navbar() {
   return (
-   
-
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid p-2">
-        <a class="navbar-brand" href="#">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary ">
+      <div className="container-fluid p-2" >
+        <a className="navbar-brand" href="#">
           <img src={logo} style={{ height: "20px" }} />
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <Link class="nav-link active" aria-current="page" to="/signup">Signup</Link>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 p-20px mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/signup">Signup</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/about">About</Link>
             </li>
 
-            <li class="nav-item">
-              <Link class="nav-link active" aria-current="page" to="/about">About</Link>
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/products">Product</Link>
             </li>
 
-            <li class="nav-item">
-              <Link class="nav-link active" aria-current="page" to="/products">Product</Link>
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="pricing">Pricing</Link>
             </li>
-
-            <li class="nav-item">
-              <Link class="nav-link active" aria-current="page" to="pricing">Pricing</Link>
-            </li>
-            <li class="nav-item">
-              <Link class="nav-link active" aria-current="page" to="support">Support</Link>
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="support">Support</Link>
             </li>
           </ul>
-         
-         
         </div>
       </div>
     </nav>
