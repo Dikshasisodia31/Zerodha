1d24553bcba2ad1c10d01a9437c1d58b6a576f96
Dikshasisodia31 <dikshasisodia3112@gmail.com>
2026-01-30 09:29:49 +0530

    Add topbar elements

--- dashboard/src/components/Topbar/Topbar.jsx
+++ dashboard/src/components/Topbar/Topbar.jsx
@@ -4,19 +4,20 @@ import Menu from "../Menu/Menu";
 function Topbar() {
     return ( 
     <div className="topbar-container border-bottom">
-        <div className="row">
-        <div className="col-8"> <Menu/></div>
-        <div className=" col -2 top-container">
-            <p>NIFTY 50</p>
-            <p>100.2</p>
-            <p></p>
+        <div className="indices-container">
+        <div className="nifty">
+          <p className="index">NIFTY 50</p>
+          <p className="index-points">{100.2} </p>
+          <p className="percent"> </p>
         </div>
-        <div className="col-2 sensex">
-            <p></p>
-            <p>SENSEX</p>
-            <p>100.2</p>
+        <div className="sensex">
+          <p className="index">SENSEX</p>
+          <p className="index-points">{100.2}</p>
+          <p className="percent"></p>
         </div>
-        </div>
+      </div>
+
+      <Menu />
     </div> 
     );
 }
